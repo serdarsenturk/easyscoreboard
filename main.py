1d04a4e06ee30ca04e8fbe968595be7f3afb7ea6
Serdar <serdarsenturk@windowslive.con>
2021-02-11 19:45:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-print("Hello World")
+@app.route('/')
+def homeView():
+    return "<h1>Welcome to easyscoreboard.</h1>"
 
 @app.route('/api/v1/hello', methods = ["GET"])
 def printHello():
