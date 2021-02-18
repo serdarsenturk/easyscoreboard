fb8670d3e5def42abca33b41398c2d4306fa90d3
Serdar <serdarsenturk@windowslive.con>
2021-02-18 11:10:54 +0300

    Add flask_sqlalchemy ORM modulo and configurations.

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:****@localhost:5432/easyscoreboard'
+db = SQLAlchemy(app)
 
 @app.route('/')
 def homeView():
