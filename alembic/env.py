38dc6a941e1ec3795a74e17b8a5f4b4712d18327
Serdar <serdarsenturk@windowslive.con>
2021-02-18 22:20:37 +0300

    Get DB_CONNECTION_STRING with environment variable

--- alembic/env.py
+++ alembic/env.py
@@ -9,6 +9,7 @@ import os
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+config.set_main_option('sqlalchemy.url', os.environ['DB_CONNECTION_STRING'])
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
