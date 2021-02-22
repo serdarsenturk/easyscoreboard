429d1c2f6bdc59ea80287c14c2e28e07e96d3ad8
Serdar <serdarsenturk@windowslive.con>
2021-02-22 10:15:14 +0300

    Create ScoreBoard model

---
+++ app/models/scoreboard.py
@@ -0,0 +1,13 @@
+#Note how we never defined a __init__ method on the ScoreBoard class?
+#That’s because SQLAlchemy adds an implicit constructor to all
+#model classes which accepts keyword arguments for all its columns and relationships.
+
+from app import db
+
+class ScoreBoard(db.Model):
+    __tablename__ = 'score_boards'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<ScoreBoard %r>' % self.name
