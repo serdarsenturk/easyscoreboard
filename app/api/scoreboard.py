ed94906028bfb0f0fc851717b42b918189a94d10
Serdar <serdarsenturk@windowslive.con>
2021-02-22 14:33:56 +0300

    Add GET api/v1/scoreboards route

--- app/api/scoreboard.py
+++ app/api/scoreboard.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify
 
 from app.models.scoreboard import ScoreBoard
+from app.schema.scoreboard import score_boards_schema
 
 scoreboard = Blueprint('scoreboard', __name__, url_prefix='/api/v1/')
 
@@ -8,3 +9,5 @@ scoreboard = Blueprint('scoreboard', __name__, url_prefix='/api/v1/')
 def readById():
     score_board = ScoreBoard.query.get(1)
     return jsonify(id=score_board.id, name=score_board.name)
+
+@scoreboard.route('scoreboards', methods=["GET"])
