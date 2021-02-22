cbf737630c50a9a7e94c2ff5179105528268b3c7
Serdar <serdarsenturk@windowslive.con>
2021-02-22 18:42:20 +0300

    Use score_board_schema for GET api/v1/hello route

--- app/api/scoreboard.py
+++ app/api/scoreboard.py
@@ -1,14 +1,14 @@
 from flask import Blueprint, jsonify
 
 from app.models.scoreboard import ScoreBoard
-from app.schema.scoreboard import score_boards_schema
+from app.schema.scoreboard import score_boards_schema, score_board_schema
 
 scoreboard = Blueprint('scoreboard', __name__, url_prefix='/api/v1/')
 
 @scoreboard.route('hello', methods=["GET"])
 def readById():
     score_board = ScoreBoard.query.get(1)
-    return jsonify(id=score_board.id, name=score_board.name)
+    return jsonify(score_board_schema.dump(score_board))
 
 @scoreboard.route('scoreboards', methods=["GET"])
 def return_score_boards():
