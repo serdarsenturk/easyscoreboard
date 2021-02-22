183bb786e9d6904e9835859117a196033aa4ab07
Serdar <serdarsenturk@windowslive.con>
2021-02-22 10:50:42 +0300

    Move homeView function and their route home.py

--- app/api/scoreboard.py
+++ app/api/scoreboard.py
@@ -2,13 +2,9 @@ from flask import Blueprint, jsonify
 
 from app.models.scoreboard import ScoreBoard
 
-scoreboard = Blueprint('scoreboard', __name__, url_prefix='/')
+scoreboard = Blueprint('scoreboard', __name__, url_prefix='/api/v1/')
 
-@scoreboard.route('/')
-def homeView():
-    return "<h1>Welcome to Easy Score Board.</h1>"
-
-@scoreboard.route('api/v1/hello', methods=["GET"])
+@scoreboard.route('hello', methods=["GET"])
 def readById():
     score_board = ScoreBoard.query.get(1)
     return jsonify(id=score_board.id, name=score_board.name)
