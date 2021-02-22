4a3c7f8dcce12747be865d196b8e71ceded093b6
Serdar Senturk <serdarsenturk@windowslive.com>
2021-02-22 21:34:22 +0300

    Get scoreboards by (#6)
    * Change url 'hello' to 'scoreboard'/<id>
    
    * Create readById function and take id paramater from route
    
    * Return scoreboards with 'id' paramater
    
    Co-authored-by: Serdar <serdarsenturk@windowslive.con>

--- app/api/scoreboard.py
+++ app/api/scoreboard.py
@@ -5,9 +5,9 @@ from app.schema.scoreboard import score_boards_schema, score_board_schema
 
 scoreboard = Blueprint('scoreboard', __name__, url_prefix='/api/v1/')
 
-@scoreboard.route('hello', methods=["GET"])
-def readById():
-    score_board = ScoreBoard.query.get(1)
+@scoreboard.route('scoreboards/<id>', methods=["GET"])
+def readById(id):
+    score_board = ScoreBoard.query.get(id)
     return jsonify(score_board_schema.dump(score_board))
 
 @scoreboard.route('scoreboards', methods=["GET"])
