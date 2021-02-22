cb30a9564ce3f116ff402e282bfec91887206a17
Serdar Senturk <serdarsenturk@windowslive.com>
2021-02-22 20:26:29 +0300

    Return all scoreboards (#5)
    * Import Marshmallow module
    
    * Add GET api/v1/scoreboards route
    
    * Create function to return all scoreboards
    
    * Create ScoreBoardSchema class
    
    * Add Marshmallow modules to requirements.txt
    
    * Use score_board_schema for GET api/v1/hello route
    
    Co-authored-by: Serdar <serdarsenturk@windowslive.con>

--- app/schema/scoreboard.py
+++ app/schema/scoreboard.py
@@ -1,1 +1,9 @@
-#ScoreBoard schema
+from app import ma
+from app.models.scoreboard import ScoreBoard
+
+class ScoreBoardSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = ScoreBoard
+
+score_board_schema = ScoreBoardSchema()
+score_boards_schema = ScoreBoardSchema(many=True)
