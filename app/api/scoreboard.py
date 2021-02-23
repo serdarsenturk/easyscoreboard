4c8b21f6865964d332d2780c0dfd37e7c1254950
Serdar <serdarsenturk@windowslive.con>
2021-02-23 14:17:48 +0300

    Re-name methods name

--- app/api/scoreboard.py
+++ app/api/scoreboard.py
@@ -6,17 +6,17 @@ from app.schema.scoreboard import scoreboards_schema, scoreboard_schema
 scoreboard = Blueprint('scoreboard', __name__, url_prefix='/api/v1/scoreboards')
 
 @scoreboard.route('<id>', methods=["GET"])
-def readById(id):
+def get_scoreboard_by_id(id):
     score_board = ScoreBoard.query.get(id)
     return jsonify(scoreboard_schema.dump(score_board))
 
 @scoreboard.route('/', methods=["GET"])
-def return_scoreboards():
+def list_scoreboards():
     scoreboards = ScoreBoard.query.all()
     return jsonify(scoreboards_schema.dump(scoreboards))
 
 @scoreboard.route('/', methods=["POST"])
-def post_scoreboards():
+def create_scoreboard():
     name = request.json['name']
 
     new_scoreboard = ScoreBoard(name=name)
