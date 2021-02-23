3f4237487e384ffbc68298b845c4713e1e380920
Serdar <serdarsenturk@windowslive.con>
2021-02-23 11:33:33 +0300

    Update `id` column restricts

--- app/models/scoreboard.py
+++ app/models/scoreboard.py
@@ -6,5 +6,5 @@ from app import db
 
 class ScoreBoard(db.Model):
     __tablename__ = 'score_boards'
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
