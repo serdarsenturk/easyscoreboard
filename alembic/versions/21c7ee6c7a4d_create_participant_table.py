c6c4b61cf68b5566fea462323f83098a7aaa4c54
Serdar <serdarsenturk@windowslive.con>
2021-02-24 14:03:34 +0300

    Create participant_table with alembic

---
+++ alembic/versions/21c7ee6c7a4d_create_participant_table.py
@@ -0,0 +1,30 @@
+"""create participant table
+
+Revision ID: 21c7ee6c7a4d
+Revises: caf1dcc1f95b
+Create Date: 2021-02-24 13:15:53.327813
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '21c7ee6c7a4d'
+down_revision = 'caf1dcc1f95b'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table(
+        'participant',
+        sa.Column('prt_id', sa.Integer(), primary_key=True),
+        sa.Column('name', sa.VARCHAR(25), nullable=False),
+        sa.Column('team_id', sa.Integer()),
+        sa.PrimaryKeyConstraint('prt_id')
+    )
+
+
+def downgrade():
+    op.drop_table('participant')
