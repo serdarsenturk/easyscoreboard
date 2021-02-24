b5ed5374f20e4866f2eaf91f09bf046749f8b43b
Serdar <serdarsenturk@windowslive.con>
2021-02-24 21:50:29 +0300

    Change nullable variable false to true

--- app/models/scoreboard.py
+++ app/models/scoreboard.py
@@ -6,6 +6,8 @@ from app import db
 
 class ScoreBoard(db.Model):
     __tablename__ = 'score_boards'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = db.Column(db.String(80), unique=True, nullable=False)
 
 class Participant(db.Model):
     __tablename__ = 'participant'
