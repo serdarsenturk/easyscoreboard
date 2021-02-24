9d67bddc2012bde4b86416496cfab81a736703d4
Serdar <serdarsenturk@windowslive.con>
2021-02-24 21:48:52 +0300

    Create ParticipantsSchema

--- app/schema/scoreboard.py
+++ app/schema/scoreboard.py
@@ -1,9 +1,17 @@
 from app import ma
-from app.models.scoreboard import ScoreBoard
+from app.models.scoreboard import ScoreBoard, Participant
+
 
 class ScoreBoardSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = ScoreBoard
 
+class ParticipantSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Participant
+
 scoreboard_schema = ScoreBoardSchema()
 scoreboards_schema = ScoreBoardSchema(many=True)
+
+participant_schema = ParticipantSchema()
+participants_schema = ParticipantSchema(many=True)
