3de993df1ab942d57b7f4d8101c2b9bc3577c575
Serdar <serdarsenturk@windowslive.con>
2021-02-25 14:56:26 +0300

    Update __table__name

--- app/models/participant.py
+++ app/models/participant.py
@@ -1,7 +1,7 @@
 from app import db
 
 class Participant(db.Model):
-    __tablename__ = 'participant'
+    __tablename__ = 'participants'
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     name = db.Column(db.VARCHAR(25), unique=True, nullable=False)
     board_id = db.Column(db.Integer, db.ForeignKey('score_boards.id'), nullable=False)
