ed20f3481e99ee4bf07bc9f113c186a1ead9db1e
Serdar <serdarsenturk@windowslive.con>
2021-02-25 12:34:29 +0300

    Adjust `server_default=0` constant for score column.

--- app/models/participant.py
+++ app/models/participant.py
@@ -5,3 +5,4 @@ class Participant(db.Model):
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     name = db.Column(db.VARCHAR(25), unique=True, nullable=False)
     board_id = db.Column(db.Integer, db.ForeignKey('score_boards.id'), nullable=False)
+    score = db.Column(db.Integer, server_default='0')
