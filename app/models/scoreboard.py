2baf651bb006d2e0b8e044d590fac34dd0f84ddd
Serdar <serdarsenturk@windowslive.con>
2021-02-25 11:28:33 +0300

    Remove `Participant` model

--- app/models/scoreboard.py
+++ app/models/scoreboard.py
@@ -8,9 +8,3 @@ class ScoreBoard(db.Model):
     __tablename__ = 'score_boards'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
     name = db.Column(db.String(80), unique=True, nullable=False)
-
-class Participant(db.Model):
-    __tablename__ = 'participant'
-    prt_id = db.Column(db.Integer, primary_key=True, nullable=False)
-    name = db.Column(db.VARCHAR(25), unique=True, nullable=False)
-    board_id = db.Column(db.Integer, db.ForeignKey('score_boards.id'), nullable=False)
