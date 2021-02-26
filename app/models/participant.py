877c0832bdfabd9dfc30a810edd2383b34e75392
Serdar <serdarsenturk@windowslive.con>
2021-02-26 15:31:46 +0300

    Update board_id column Foreignkey table

--- app/models/participant.py
+++ app/models/participant.py
@@ -4,5 +4,5 @@ class Participant(db.Model):
     __tablename__ = 'participants'
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     name = db.Column(db.VARCHAR(25), nullable=False)
-    board_id = db.Column(db.Integer, db.ForeignKey('score_boards.id'), nullable=False)
+    board_id = db.Column(db.Integer, db.ForeignKey('boards.id'), nullable=False)
     score = db.Column(db.Integer, unique=True, server_default='0')
