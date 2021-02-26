5c6fd7b032650f9167cf3579fb7d370007a57f51
Serdar <serdarsenturk@windowslive.con>
2021-02-26 15:31:05 +0300

    Re-name scoreboard model name to board

--- app/models/board.py
+++ app/models/board.py
@@ -1,6 +1,6 @@
 from app import db
 
-class ScoreBoard(db.Model):
-    __tablename__ = 'score_boards'
+class Board(db.Model):
+    __tablename__ = 'boards'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
     name = db.Column(db.String(80), nullable=False)
