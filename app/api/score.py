48858d0a673052fb3144f2ec9d73a263986e2008
Serdar <serdarsenturk@windowslive.con>
2021-02-26 13:00:53 +0300

    Update route and `add_score_by_id` method

--- app/api/score.py
+++ app/api/score.py
@@ -1,14 +1,19 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from app import db
 from app.models.participant import Participant
+from app.models.scoreboard import ScoreBoard
 from app.schema.participant import participant_schema
 
 scores = Blueprint('scores', __name__, url_prefix='/api/v1/scoreboards/<board_id>/participants/<id>/score')
 
-@scores.route('/<int:increment>', methods=["PUT"])
-def add_score_by_id(id, increment):
-    participant = Participant.query.get(id)
+@scores.route('', methods=["PUT"])
+def add_score_by_id(id, board_id):
+    participant = Participant.query.join(ScoreBoard)\
+        .filter(ScoreBoard.id == board_id)\
+        .filter(Participant.id == id)\
+        .first()
 
+    increment = request.json['increment']
     participant.score += increment  # Increment to score by `increment` variable where come from function parameter
 
     db.session.commit()
