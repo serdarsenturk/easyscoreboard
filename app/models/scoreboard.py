61ff69460c0a167785359ed11afc03de70405b4a
Serdar <serdarsenturk@windowslive.con>
2021-02-26 12:59:30 +0300

    Remove unique constraint

--- app/models/scoreboard.py
+++ app/models/scoreboard.py
@@ -3,4 +3,4 @@ from app import db
 class ScoreBoard(db.Model):
     __tablename__ = 'score_boards'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
-    name = db.Column(db.String(80), unique=True, nullable=False)
+    name = db.Column(db.String(80), nullable=False)
