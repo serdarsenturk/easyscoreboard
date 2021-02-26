a73d8531206bfb0e4abc74decd8b67f9ac3e9f4e
Serdar <serdarsenturk@windowslive.con>
2021-02-26 12:59:48 +0300

    Re-add unique constraint

--- app/models/participant.py
+++ app/models/participant.py
@@ -5,4 +5,4 @@ class Participant(db.Model):
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     name = db.Column(db.VARCHAR(25), nullable=False)
     board_id = db.Column(db.Integer, db.ForeignKey('score_boards.id'), nullable=False)
-    score = db.Column(db.Integer, server_default='0')
+    score = db.Column(db.Integer, unique=True, server_default='0')
