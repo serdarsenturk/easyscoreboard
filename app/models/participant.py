80fe48c80f88bed56e8c0f94cf134b1c9f9e2e81
Serdar <serdarsenturk@windowslive.con>
2021-02-26 12:18:32 +0300

    Remove `unique constraint` option from name column

--- app/models/participant.py
+++ app/models/participant.py
@@ -3,6 +3,6 @@ from app import db
 class Participant(db.Model):
     __tablename__ = 'participants'
     id = db.Column(db.Integer, primary_key=True, nullable=False)
-    name = db.Column(db.VARCHAR(25), unique=True, nullable=False)
+    name = db.Column(db.VARCHAR(25), nullable=False)
     board_id = db.Column(db.Integer, db.ForeignKey('score_boards.id'), nullable=False)
     score = db.Column(db.Integer, server_default='0')
