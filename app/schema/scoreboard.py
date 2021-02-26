24d00d701800ea1f1bd539442025f920ae928f29
Serdar <serdarsenturk@windowslive.con>
2021-02-26 15:31:24 +0300

    Re-name scoreboards schema name to board

--- app/schema/scoreboard.py
+++
@@ -1,9 +0,0 @@
-from app import ma
-from app.models.scoreboard import ScoreBoard
-
-class ScoreBoardSchema(ma.SQLAlchemyAutoSchema):
-    class Meta:
-        model = ScoreBoard
-
-scoreboard_schema = ScoreBoardSchema()
-scoreboards_schema = ScoreBoardSchema(many=True)
