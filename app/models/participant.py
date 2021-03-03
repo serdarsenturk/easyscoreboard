2d725ce3dd1a2375b10e45c7f1119f548fd02432
Serdar <serdarsenturk@windowslive.con>
2021-03-03 12:06:46 +0300

    Configure project

--- app/models/participant.py
+++ app/models/participant.py
@@ -2,7 +2,11 @@ from app import db
 
 class Participant(db.Model):
     __tablename__ = 'participants'
+    __table_args__ = (
+        db.UniqueConstraint('board_id', 'name', name='unq_board_id_name'),
+    )
+
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     name = db.Column(db.VARCHAR(25), nullable=False)
     board_id = db.Column(db.Integer, db.ForeignKey('boards.id'), nullable=False)
-    score = db.Column(db.Integer, unique=True, server_default='0')
+    score = db.Column(db.Integer, server_default='0')
