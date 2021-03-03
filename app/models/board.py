a6f29c5932441d865105ae30e571e44321c0826f
Serdar <serdarsenturk@windowslive.con>
2021-03-03 17:13:39 +0300

    Add participants column

--- app/models/board.py
+++ app/models/board.py
@@ -1,6 +1,8 @@
 from app import db
+from app.models.participant import Participant
 
 class Board(db.Model):
     __tablename__ = 'boards'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
     name = db.Column(db.String(80), nullable=False)
+    participants = db.relationship("Participant", backref="participant")
