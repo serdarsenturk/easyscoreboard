3d3dbc8f1622c23d21e76568485f0119c01b52b5
Serdar <serdarsenturk@windowslive.con>
2021-03-08 14:47:27 +0300

    Add include_fk=True properties

--- app/schema/board.py
+++ app/schema/board.py
@@ -4,6 +4,7 @@ from app.models.board import Board
 class BoardSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = Board
+        include_fk = True
 
 board_schema = BoardSchema()
 boards_schema = BoardSchema(many=True)
