ca8c85d22fb744fa99054dd139b61800c5b0c057
Serdar <serdarsenturk@windowslive.con>
2021-03-19 00:24:28 +0300

    Add CORS options to score api

--- app/api/score.py
+++ app/api/score.py
@@ -1,11 +1,15 @@
 from flask import Blueprint, jsonify, request
+from flask_cors import CORS, cross_origin
+
 from app import db
 from app.models.participant import Participant
 from app.schema.participant import participant_schema
 
 scores = Blueprint('scores', __name__, url_prefix='/api/v1/boards/<board_id>/participants/<id>/score')
+CORS(scores)
 
 @scores.route('', methods=["PUT"])
+@cross_origin(supports_credentials=True)
 def add_score_by_id(id, board_id):
     participant = db.session.query(Participant)\
         .filter(Participant.board_id == board_id)\
