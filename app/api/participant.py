a6c236e1e89c2b9b81611808d63d5e953cf3a0d5
Serdar <serdarsenturk@windowslive.con>
2021-03-19 22:28:22 +0300

    Revert "Add CORS options to participant api"
    This reverts commit 89c5a22e93f7aa4df8af400dc6c492fba1405ba3.

--- app/api/participant.py
+++ app/api/participant.py
@@ -1,14 +1,11 @@
 from flask import Blueprint, request, jsonify
-from flask_cors import CORS, cross_origin
 from app import db
 from app.models.participant import Participant
 from app.schema.participant import participant_schema, participants_schema
 
 participants = Blueprint('participants', __name__, url_prefix='/api/v1/boards/<board_id>/participants')
-CORS(participants)
 
 @participants.route('', methods=["POST"])
-@cross_origin(supports_credentials=True)
 def create_participants(board_id):
     name = request.json['name']
 
@@ -19,7 +16,6 @@ def create_participants(board_id):
     return participant_schema.dump(participant)
 
 @participants.route('', methods=["GET"])
-@cross_origin(supports_credentials=True)
 def list_participants(board_id):
     participants = db.session.query(Participant)\
         .filter(Participant.board_id == board_id)\
@@ -28,7 +24,6 @@ def list_participants(board_id):
     return jsonify(participants_schema.dump(participants))
 
 @participants.route('/<id>', methods=["DELETE"])
-@cross_origin(supports_credentials=True)
 def remove_participants_by_id(id, board_id):
     participant = db.session.query(Participant)\
         .filter(Participant.board_id == board_id)\
@@ -41,7 +36,6 @@ def remove_participants_by_id(id, board_id):
     return ('', 204)
 
 @participants.route('/<id>/name', methods=["PUT"])
-@cross_origin(supports_credentials=True)
 def modify_participants_by_id(id, board_id):
     participant = db.session.query(Participant)\
         .filter(Participant.board_id == board_id)\
