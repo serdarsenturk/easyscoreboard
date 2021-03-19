80b1ee00df74b3d215488313ee08f912c6e44e08
Serdar <serdarsenturk@windowslive.con>
2021-03-19 22:30:23 +0300

    Update `app/api/score.py`

--- app/api/score.py
+++ app/api/score.py
@@ -1,14 +1,11 @@
 import os
-
 from flask import Blueprint, jsonify, request
-from flask_cors import CORS, cross_origin
 from pusher import Pusher
 from app import db
 from app.models.participant import Participant
 from app.schema.participant import participant_schema
 
 scores = Blueprint('scores', __name__, url_prefix='/api/v1/boards/<board_id>/participants/<id>/score')
-CORS(scores)
 
 pusher = Pusher(
     app_id=os.environ.get('PUSHER_APP_ID'),
@@ -19,7 +16,6 @@ pusher = Pusher(
 )
 
 @scores.route('', methods=["PUT"])
-@cross_origin(supports_credentials=True)
 def add_score_by_id(id, board_id):
     participant = db.session.query(Participant)\
         .filter(Participant.board_id == board_id)\
