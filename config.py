9ba1f46193d48da9fd5f141f0daf6749c87e1867
Serdar <serdarsenturk@windowslive.con>
2021-03-23 16:14:21 +0300

    Add `Pusher` configurations in config.py

--- config.py
+++ config.py
@@ -4,3 +4,7 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ['DB_CONNECTION_STRING']
     SQLALCHEMY_ECHO = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    PUSHER_APP_ID = os.environ['PUSHER_APP_ID']
+    PUSHER_KEY = os.environ['PUSHER_KEY']
+    PUSHER_SECRET = os.environ['PUSHER_SECRET']
+    PUSHER_CLUSTER = os.environ['PUSHER_CLUSTER']
