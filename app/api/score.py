3dd7e359331fd942b0805076b8fda50acbcb7dfe
Serdar Senturk <serdarsenturk@windowslive.com>
2021-03-23 19:00:49 +0300

    Pusher configuration (#15)
    * Add CORS options to board api
    
    * Add CORS options to participant api
    
    * Add CORS options to score api
    
    * Add pusher dependencies
    
    * Import Pusher module
    
    * Add pusher configurations
    
    * Publish `score-added` event on `score` channel
    
    * Change variables value
    
    * Get pusher configurations from .env file
    
    * Re-name `channel` name to `score-updated` and `event-name` to participant-{participant.id}
    
    * Revert "Add CORS options to board api"
    
    This reverts commit bcaf1dde81dbc50b68a5e0d4f3875e082ab216fe.
    
    * Revert "Add CORS options to participant api"
    
    This reverts commit 89c5a22e93f7aa4df8af400dc6c492fba1405ba3.
    
    * Update `app/api/score.py`
    
    * Add `Pusher` configurations in config.py
    
    * Use app.config property instead of os.envrion.get property
    
    * Remove `PUSHER_CLUSTER` environment variable
    
    * Do hard-code `PUSHER_CLUSTER` variable and set to `eu`
    
    * Revert "Add pusher dependencies"
    
    This reverts commit c80d4cdb12df54101cb704b24474b3a49e517726.
    
    * Add Pusher depedencies
    
    Co-authored-by: Serdar <serdarsenturk@windowslive.con>

--- app/api/score.py
+++ app/api/score.py
@@ -1,9 +1,18 @@
 from flask import Blueprint, jsonify, request
-from app import db
+from pusher import Pusher
+from app import db, app
 from app.models.participant import Participant
 from app.schema.participant import participant_schema
 
 scores = Blueprint('scores', __name__, url_prefix='/api/v1/boards/<board_id>/participants/<id>/score')
+
+pusher = Pusher(
+    app_id=app.config.get('PUSHER_APP_ID'),
+    key=app.config.get('PUSHER_KEY'),
+    secret=app.config.get('PUSHER_SECRET'),
+    cluster='eu',
+    ssl=True
+)
 
 @scores.route('', methods=["PUT"])
 def add_score_by_id(id, board_id):
@@ -16,5 +25,5 @@ def add_score_by_id(id, board_id):
     participant.score += increment  # Increment to score by `increment` variable where come from function parameter
 
     db.session.commit()
-
+    pusher.trigger(f"participant-{participant.id}", 'score-updated', participant.score)
     return jsonify(participant_schema.dump(participant))
--- config.py
+++ config.py
@@ -4,3 +4,6 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ['DB_CONNECTION_STRING']
     SQLALCHEMY_ECHO = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    PUSHER_APP_ID = os.environ['PUSHER_APP_ID']
+    PUSHER_KEY = os.environ['PUSHER_KEY']
+    PUSHER_SECRET = os.environ['PUSHER_SECRET']
