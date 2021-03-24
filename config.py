8f854504ec9a893c5d557a868a7e514a1ae40e55
Serdar <serdarsenturk@windowslive.con>
2021-03-24 16:25:35 +0300

    Add `ORIGINS` environment variable

--- config.py
+++ config.py
@@ -7,3 +7,4 @@ class Config:
     PUSHER_APP_ID = os.environ['PUSHER_APP_ID']
     PUSHER_KEY = os.environ['PUSHER_KEY']
     PUSHER_SECRET = os.environ['PUSHER_SECRET']
+    ORIGINS = os.environ['ORIGINS']
