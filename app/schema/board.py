fbf6a6e0757b223177f3a3217af4a21716b91606
Serdar <serdarsenturk@windowslive.con>
2021-04-05 17:22:17 +0300

    Add `code` fields in board schema

--- app/schema/board.py
+++ app/schema/board.py
@@ -1,5 +1,4 @@
 from marshmallow import fields
-
 from app import ma
 from app.schema.participant import ParticipantSchema
 
@@ -7,6 +6,7 @@ from app.schema.participant import ParticipantSchema
 class BoardSchema(ma.SQLAlchemyAutoSchema):
     id = fields.Int(dump_only=True)
     name = fields.Str()
+    code = fields.Str()
     participants = fields.Nested(ParticipantSchema, many=True)
 
 board_schema = BoardSchema()
