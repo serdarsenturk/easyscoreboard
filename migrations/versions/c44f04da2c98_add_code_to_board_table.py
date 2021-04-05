f1441a4c7faa3c2a5f0b61703e0e84cabd27d97e
Serdar <serdarsenturk@windowslive.con>
2021-04-05 17:21:14 +0300

    Add code column both `participants` and `boards` tables

---
+++ migrations/versions/c44f04da2c98_add_code_to_board_table.py
@@ -0,0 +1,24 @@
+"""add-code-to-board-table
+
+Revision ID: c44f04da2c98
+Revises: 
+Create Date: 2021-04-04 13:47:17.866463
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'c44f04da2c98'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
