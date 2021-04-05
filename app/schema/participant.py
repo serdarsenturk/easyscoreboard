e65547f635e51681d5c9c5122ddfcc13adfda0d3
Serdar <serdarsenturk@windowslive.con>
2021-04-05 17:21:52 +0300

    Pass `code`, `name` and `score` fields in ParticipantSchema

--- app/schema/participant.py
+++ app/schema/participant.py
@@ -4,6 +4,7 @@ from app.models.participant import Participant
 class ParticipantSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = Participant
+        fields = ('code', 'name', 'score')
 
 participant_schema = ParticipantSchema()
 participants_schema = ParticipantSchema(many=True)
