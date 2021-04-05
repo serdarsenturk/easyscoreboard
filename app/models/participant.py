10ca5ff887e8e5a1c8e6d5fb2cdef2d44d02c4f2
Serdar <serdarsenturk@windowslive.con>
2021-04-05 17:22:58 +0300

    Add `code` column in participant model

--- app/models/participant.py
+++ app/models/participant.py
@@ -8,5 +8,6 @@ class Participant(db.Model):
 
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     name = db.Column(db.VARCHAR(25), nullable=False)
+    code = db.Column(db.String(8))
     board_id = db.Column(db.Integer, db.ForeignKey('boards.id'), nullable=False)
     score = db.Column(db.Integer, server_default='0')
