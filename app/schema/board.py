6676f62bab42c944a48582a8bf47a4190c64ab12
Serdar <serdarsenturk@windowslive.con>
2021-04-06 20:39:42 +0300

    Remove class Meta

--- app/schema/board.py
+++ app/schema/board.py
@@ -1,13 +1,6 @@
+from marshmallow import fields
 from app import ma
-from app.models.board import Board
 from app.schema.participant import ParticipantSchema
 
-class BoardSchema(ma.SQLAlchemySchema):
-    class Meta:
-        model = Board
-
-    code = ma.auto_field()
-    name = ma.auto_field()
-    participants = ma.Nested(ParticipantSchema, many=True)
 
 board_schema = BoardSchema()
