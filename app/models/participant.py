5167780bcf3827dd4656b9f7721898221fe8a16f
Serdar <serdarsenturk@windowslive.con>
2021-04-06 14:24:52 +0300

    Add nullable, unique and index constraints for `code` column

--- app/models/participant.py
+++ app/models/participant.py
@@ -8,6 +8,6 @@ class Participant(db.Model):
 
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     name = db.Column(db.VARCHAR(25), nullable=False)
-    code = db.Column(db.String(8))
+    code = db.Column(db.String(8), nullable=False, unique=True, index=True)
     board_id = db.Column(db.Integer, db.ForeignKey('boards.id'), nullable=False)
     score = db.Column(db.Integer, server_default='0')
