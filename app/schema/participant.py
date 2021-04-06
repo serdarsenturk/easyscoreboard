c73379400bfb7048f32595db38fe75b4be28d1c3
Serdar <serdarsenturk@windowslive.con>
2021-04-06 20:40:31 +0300

    Remove class Meta

--- app/schema/participant.py
+++ app/schema/participant.py
@@ -1,13 +1,6 @@
+from marshmallow import fields
 from app import ma
-from app.models.participant import Participant
 
-class ParticipantSchema(ma.SQLAlchemySchema):
-    class Meta:
-        model = Participant
-
-    code = ma.auto_field()
-    name = ma.auto_field()
-    score = ma.auto_field()
 class ParticipantSchema(ma.SQLAlchemyAutoSchema):
     code = fields.Str()
     name = fields.Str()
