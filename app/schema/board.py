b377b005e1decc18c1c404fdbc758db6ea112ff1
Serdar <serdarsenturk@windowslive.con>
2021-04-06 20:40:10 +0300

    Define fields manuel

--- app/schema/board.py
+++ app/schema/board.py
@@ -2,5 +2,9 @@ from marshmallow import fields
 from app import ma
 from app.schema.participant import ParticipantSchema
 
+class BoardSchema(ma.SQLAlchemyAutoSchema):
+    name = fields.Str()
+    code = fields.Str()
+    participants = fields.Nested(ParticipantSchema, many=True)
 
 board_schema = BoardSchema()
