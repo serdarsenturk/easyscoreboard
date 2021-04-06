2544ab6ada636e79a9d8efa938576868c84f8d98
Serdar <serdarsenturk@windowslive.con>
2021-04-06 14:32:29 +0300

    Create handle_exception method to catch HTTPExceptions

--- app/__init__.py
+++ app/__init__.py
@@ -31,6 +31,21 @@ app.register_blueprint(home)
 
 #Register error handlers
 app.register_error_handler(404, handle_not_found)
+
+@app.errorhandler(HTTPException)
+def handle_exception(e):
+    """Return JSON instead of HTML for HTTP errors."""
+    # start with the correct headers and status code from the error
+    response = e.get_response()
+    # replace the body with JSON
+    response.data = json.dumps({
+        "code": e.code,
+        "name": e.name,
+        "description": e.description,
+    })
+    response.content_type = "application/json"
+    return response
+
 # Build the database:
 # This will create the database file using SQLAlchemy
 db.create_all()
