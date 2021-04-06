934fe764919f340fc6d6222d0fde431dd1fde7a9
Serdar <serdarsenturk@windowslive.con>
2021-04-06 20:40:20 +0300

    Define fields manuel

--- app/schema/participant.py
+++ app/schema/participant.py
@@ -8,6 +8,10 @@ class ParticipantSchema(ma.SQLAlchemySchema):
     code = ma.auto_field()
     name = ma.auto_field()
     score = ma.auto_field()
+class ParticipantSchema(ma.SQLAlchemyAutoSchema):
+    code = fields.Str()
+    name = fields.Str()
+    score = fields.Int()
 
 participant_schema = ParticipantSchema()
 participants_schema = ParticipantSchema(many=True)
