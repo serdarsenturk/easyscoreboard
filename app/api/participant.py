59a56de973f94e0a2c5f487c0b7e26ca926e0359
Serdar <serdarsenturk@windowslive.con>
2021-04-06 15:05:33 +0300

    Re-name `remove_participants_by_id` to `remove_participants_by_code

--- app/api/participant.py
+++ app/api/participant.py
@@ -46,11 +46,11 @@ def list_participants(board_code):
     return jsonify(participants_schema.dump(participant_list))
 
 @participants.route('/<code>', methods=["DELETE"])
-def remove_participants_by_id(code, board_code):
     try:
         is_valid = db.session.query(Board)\
         .filter(Board.code == board_code)\
         .first()
+def remove_participants_by_code(code, board_code):
 
         if is_valid:
             participant = db.session.query(Participant) \
