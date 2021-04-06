dd0b8d95e9fca5b2fb6e26f6615619dcff763c0f
Serdar <serdarsenturk@windowslive.con>
2021-04-06 14:26:36 +0300

    Add nullable, unique and index constraints for `board table`'s `code` column

--- app/models/board.py
+++ app/models/board.py
@@ -4,5 +4,5 @@ class Board(db.Model):
     __tablename__ = 'boards'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
     name = db.Column(db.String(80), nullable=False)
-    code = db.Column(db.String(8))
+    code = db.Column(db.String(8), nullable=False, unique=True, index=True)
     participants = db.relationship("Participant", backref="participant")
